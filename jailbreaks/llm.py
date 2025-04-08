9ea355054202c5f54d55eece2453238e82477599
Jonas Rosengren <rosengrenjonas98@gmail.com>
2025-04-08 16:37:09 +0200

    wandb

--- jailbreaks/llm.py
+++ jailbreaks/llm.py
@@ -3,40 +3,46 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 from typing import List, Dict
 
-from jailbreaks.methods.base_method import JailBreakMethod, PromptInjection, GenerationExploit
+from jailbreaks.methods.base_method import JailBreakMethod, PromptInjection, GenerationExploit, ModelManipulation
 
 import logging
 logger = logging.getLogger("LLM")
 
+from jailbreaks.utils.model_loading import load_model, load_tokenizer
+
+from jailbreaks.utils.tokenization import format_prompts, tokenize_prompts
+
 class LLM:
-    def __init__(self, model: AutoModelForCausalLM, methods: List[JailBreakMethod], device: str = "cpu"):
-        self.model = model.to(device)
-        self.name = getattr(model.config, "name_or_path", None) or model.config._name_or_path
-        self.tokenizer = AutoTokenizer.from_pretrained(self.name)
+    def __init__(self, model_path: str, methods: List[JailBreakMethod], device: str = "cpu"):
+        self.model = load_model(model_path)
+        self.name = model_path
+        self.tokenizer = load_tokenizer(model_path)
         self.methods = methods
         self.device = device
     
     def to(self, device):
-        # Only move if needed
         if self.device != device:
             self.device = device
             if not hasattr(self.model, "device_map") or self.model.device_map is None:
                 self.model = self.model.to(device)
         return self
-
+    
     def _process_prompt(self, prompt: str) -> Dict[str, torch.Tensor]:
-        
-        logger.debug(f"modified prompt: {prompt}")
-        # tokenized and batch prepared inputs
-        inputs = self.tokenizer(
-            prompt, 
-            return_tensors="pt", 
-            padding=True, 
-            truncation=True,
-            max_length=2048
-        )
-        # move each batch to the correct device
-        return {k: v.to(self.device) for k, v in inputs.items()}
+        return self._process_prompts([prompt])[0]
+    
+    def _process_prompts(self, prompts: List[str]) -> Dict[str, torch.Tensor]:
+        # # tokenized and batch prepared inputs
+        # inputs = self.tokenizer(
+        #     prompt, 
+        #     return_tensors="pt", 
+        #     padding=True, 
+        #     truncation=True,
+        #     max_length=2048
+        # )
+        # # move each batch to the correct device
+        # return {k: v.to(self.device) for k, v in inputs.items()}
+        inputs = tokenize_prompts(format_prompts(prompts, self.tokenizer), self.tokenizer)
+        return inputs
     
     def _remove_prompt(self, prompt: str, response: str) -> str:
         return response[len(prompt):].strip()
@@ -45,8 +51,14 @@ class LLM:
         for method in self.methods:
             if isinstance(method, PromptInjection):
                 prompt = method.preprocess(prompt, self.model.config.name_or_path)
+            # TODO: fix prefix injection with chat templates
     
-        inputs = self._process_prompt(prompt)
+        inputs = tokenize_prompts(prompt, self.tokenizer)
+        
+        for method in self.methods:
+            if isinstance(method, ModelManipulation):
+                self.model = method.apply(self.name)
+                # potentially add multi hook support
         
         output_text = None
         for method in self.methods:
