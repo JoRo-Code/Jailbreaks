b329543ad7fa3e1dce8a3180fc0bdc3f67c3f319
Jonas Rosengren <rosengrenjonas98@gmail.com>
2025-05-05 16:51:00 +0200

    hiddenlayer method

--- jailbreaks/methods/prompt/__init__.py
+++ jailbreaks/methods/prompt/__init__.py
@@ -1,4 +1,5 @@
 from .prefix_injection import PrefixInjection
 from .gcg import GCG
+from .hidden_layer import HiddenLayerInjection
 
-__all__ = ["PrefixInjection", "GCG"]
+__all__ = ["PrefixInjection", "GCG", "HiddenLayerInjection"]
