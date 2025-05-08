8e6f082f2ba9a6c312dadac368cae4fe4bb19958
Jonas Rosengren <rosengrenjonas98@gmail.com>
2025-05-08 16:11:39 +0000

    add gemma and phi

--- jailbreaks/utils/tokenization.py
+++ jailbreaks/utils/tokenization.py
@@ -18,6 +18,18 @@ LLAMA3_CHAT_TEMPLATE = """<|start_header_id|>user<|end_header_id|>
 
 MISTRAL_CHAT_TEMPLATE = """[INST]{instruction}[/INST]"""
 
+PHI_4_CHAT_TEMPLATE = """
+<|im_start|>user<|im_sep|>
+{instruction}<|im_end|>
+<|im_start|>assistant<|im_sep|>
+"""
+
+GEMMA_2_CHAT_TEMPLATE = """
+<bos><start_of_turn>user
+{instruction}<end_of_turn>
+<start_of_turn>model
+"""
+
 def format_prompts(prompts: List[str], tokenizer: AutoTokenizer) -> List[str]:
     model_path = tokenizer.name_or_path.lower()
     
@@ -30,11 +42,17 @@ def format_prompts(prompts: List[str], tokenizer: AutoTokenizer) -> List[str]:
     elif "mistral" in model_path:
         logger.debug(f"Using hardcoded Mistral chat template for {tokenizer.name_or_path}")
         return [MISTRAL_CHAT_TEMPLATE.format(instruction=prompt) for prompt in prompts]
+    elif "phi" in model_path:
+        logger.debug(f"Using hardcoded Phi chat template for {tokenizer.name_or_path}")
+        return [PHI_4_CHAT_TEMPLATE.format(instruction=prompt) for prompt in prompts]
+    elif "gemma" in model_path:
+        logger.debug(f"Using hardcoded Gemma chat template for {tokenizer.name_or_path}")
+        return [GEMMA_2_CHAT_TEMPLATE.format(instruction=prompt) for prompt in prompts]
     else:
         # If no method and not a recognized model, raise an error
         raise ValueError(
             f"Tokenizer {tokenizer.name_or_path} does not have a chat_template configured "
-            f"and is not a recognized model type (Qwen/Llama). Cannot format prompts."
+            f"and is not a recognized model type. Cannot format prompts. Configure a chat template for this model in the tokenization.py file."
         )
 
 def tokenize_prompts(prompts: List[str], tokenizer: AutoTokenizer) -> List[str]:
