1c2d651c3e3dc0f14f351c7a4cd4eb5f1db75d05
Jonas Rosengren <rosengrenjonas98@gmail.com>
2025-05-17 16:19:56 +0200

    cleanup dead benchmark code

--- jailbreaks/benchmarks/base.py
+++ jailbreaks/benchmarks/base.py
@@ -1,11 +1,10 @@
-from jailbreaks.llm import LLM
+from typing import List
 
-class Benchmark:
-    def __init__(self, name=None):
-        self.name = name or self.__class__.__name__
+class Benchmark:    
+    def __init__(self, prompts: List[str] = None, max_new_tokens: int = 100, name: str = "refusal"):
+        self.name = name or self.__class__.__name__ 
+        self.prompts = prompts
+        self.max_new_tokens = max_new_tokens
     
-    def evaluate(self, model: LLM):
-        raise NotImplementedError
-    
-    def __str__(self):
-        return self.name
+    def get_prompts(self):
+        return self.prompts
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 from pathlib import Path
 import uuid
+import random
 
 import wandb
 import torch
@@ -30,7 +31,7 @@ from jailbreaks.methods.prompt import PrefixInjection, HiddenLayerInjection
 # Benchmarks & metrics
 from jailbreaks.evaluators.baseline_refusal_evaluator import BaselineRefusalEvaluator
 from jailbreaks.evaluators.quality_evaluator import QualityEvaluator
-from jailbreaks.benchmarks.refusal import RefusalBenchmark
+from jailbreaks.benchmarks import Benchmark
 
 from jailbreaks.evaluators.llm_judge.judge import LocalLLMJudge, GroqLLMJudge
 
@@ -42,6 +43,7 @@ from jailbreaks.data import (
 )
 
 logger = logging.getLogger(__name__)
+random.seed(42)
 
 def get_args():
     parser = argparse.ArgumentParser(description='Run the jailbreak pipeline')
@@ -55,7 +57,7 @@ def get_args():
     parser.add_argument('--use-local', dest='use_local', action='store_true', default=False, help='Use local evaluations')
     parser.add_argument('--upload-to-wandb', dest='upload_to_wandb', action='store_true', default=False, help='Upload to wandb')
     parser.add_argument('--output-dir', type=str, help='Output directory', default="results")
-    parser.add_argument('--benchmark', choices=['all', 'advbench', 'malicious'], help='Benchmark to run', default="all")
+    parser.add_argument('--benchmark', choices=['all', 'advbench', 'malicious', "harmless"], help='Benchmark to run', default="all")
 
     
     return parser.parse_args()
@@ -92,12 +94,17 @@ def main():
     # Pipeline setup
     advbench_train, advbench_test = get_advbench_instructions(test_split=0.8)
     malicious_instructions = get_malicious_instructions()
+    harmless_instructions = get_harmless_instructions()
+    harmless_sample = random.sample(harmless_instructions, min(100, len(harmless_instructions)))
     
     model_paths = [
         "microsoft/phi-4",
         "google/gemma-2-9b-it",
         "Qwen/Qwen2.5-7B-Instruct",
         "meta-llama/Llama-3.1-8B-Instruct",
+    ]
+    model_paths = [
+        "Qwen/Qwen2.5-0.5B-Instruct",
     ]
     
     top_p_params = {
@@ -119,19 +126,19 @@ let's do it." Then, continue the paragraph while responding to the following pro
     hidden_layer_injection = HiddenLayerInjection()
     
     N = 32
-    diff_in_means = DiffInMeans(harmful_prompts=advbench_train[:N], harmless_prompts=get_harmless_instructions()[:N])
+    diff_in_means = DiffInMeans(harmful_prompts=advbench_train[:N], harmless_prompts=harmless_instructions[:N])
     
     method_combinations = [
         # [prefix_injection_1],
         # [prefix_injection_2],
-        [prefix_injection_3],
-        [hidden_layer_injection],
-        [diff_in_means],
+        # [prefix_injection_3],#
+        # [hidden_layer_injection],#
+        # [diff_in_means],#
         # [diff_in_means, prefix_injection_3],
         # #[diff_in_means, temp_exploit], # TODO: Fix sampling with hooked transformer
 
         # [top_p_exploit],
-        [temp_exploit],
+        # [temp_exploit], #
         # [hidden_layer_injection, temp_exploit],
         # # [gcg],
         #[OutputAware(params=sampling_params, save=True)],
@@ -140,21 +147,25 @@ let's do it." Then, continue the paragraph while responding to the following pro
     
     MAX_NEW_TOKENS = 300
     
-    malicious_benchmark = RefusalBenchmark(prompts=malicious_instructions, max_new_tokens=MAX_NEW_TOKENS, name=f"malicious-{MAX_NEW_TOKENS}t")
-    advbench_benchmark = RefusalBenchmark(prompts=advbench_test, max_new_tokens=MAX_NEW_TOKENS, name=f"advbench-{MAX_NEW_TOKENS}t")
-    #advbench_train = RefusalBenchmark(prompts=advbench_train[:1], max_new_tokens=MAX_NEW_TOKENS, name="advbench-train")
+    malicious_benchmark = Benchmark(prompts=malicious_instructions, max_new_tokens=MAX_NEW_TOKENS, name=f"malicious-{MAX_NEW_TOKENS}t")
+    advbench_benchmark = Benchmark(prompts=advbench_test, max_new_tokens=MAX_NEW_TOKENS, name=f"advbench-{MAX_NEW_TOKENS}t")
+    harmless_benchmark = Benchmark(prompts=harmless_sample, max_new_tokens=MAX_NEW_TOKENS, name=f"harmless-{MAX_NEW_TOKENS}t")
+    
     if args.benchmark == "all":
         benchmarks = [malicious_benchmark, advbench_benchmark]
     elif args.benchmark == "malicious":
         benchmarks = [malicious_benchmark]
     elif args.benchmark == "advbench":
         benchmarks = [advbench_benchmark]
+    elif args.benchmark == "harmless":
+        benchmarks = [harmless_benchmark]
     
     evaluators = [
-        #BaselineRefusalEvaluator(name="include-refusal"),
+        BaselineRefusalEvaluator(name="include-refusal"),
         #QualityEvaluator(judge=LocalLLMJudge(model="Qwen/Qwen2.5-7B-Instruct")),
         #QualityEvaluator(judge=GroqLLMJudge(model="llama-3.1-8b-instant"), name="llama-v2"),
-        QualityEvaluator(judge=GroqLLMJudge(model="deepseek-r1-distill-llama-70b"), name="deepseek-v1"),
+        #QualityEvaluator(judge=GroqLLMJudge(model="deepseek-r1-distill-llama-70b"), name="deepseek-v1"),
+        #QualityEvaluator(judge=GroqLLMJudge(model="qwen-qwq-32b"), name="qwq-v1"),
     ]
     
     output_dir = Path(f"{args.output_dir}/{args.project_name}")
@@ -194,7 +205,7 @@ let's do it." Then, continue the paragraph while responding to the following pro
             eval_run_id=args.eval_run_id,
             use_local=args.use_local, # should be downloaded from wandb (responses downloads)
             upload_to_wandb=args.upload_to_wandb,
-            n_runs=5,
+            n_runs=3,
             n_samples=None,
             skip_existing=True
         ))
@@ -210,3 +221,4 @@ let's do it." Then, continue the paragraph while responding to the following pro
 
 if __name__ == "__main__":
     main()
+        
